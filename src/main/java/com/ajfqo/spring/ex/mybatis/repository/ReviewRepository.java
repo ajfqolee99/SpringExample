388fc36496fb69c9e1b27c746536a19185e8211f
DESKTOP-JOSMGD6\HY <made1999@naver.com>
2024-02-07 23:18:17 +0900

    이하영 - mybatis insert 예제 파일 추가

--- src/main/java/com/ajfqo/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/ajfqo/spring/ex/mybatis/repository/ReviewRepository.java
@@ -9,6 +9,17 @@ import com.ajfqo.spring.ex.mybatis.domain.Review;
 public interface ReviewRepository {
 	
 	// id가 3인 행 조회 후 리턴
+	// 정확히 조회되는 결과가 있어서 그 결과를 리턴되는 타입이 정확히 지정되어야함.
 	public Review selectReview(@Param("id") int id);
+	
+	// 수행된 행의 개수 return
+	public int insertReview(
+			@Param("storeId") int storeId
+			,@Param("menu") String menu
+			,@Param("userName") String userName
+			,@Param("point") double point
+			,@Param("review") String review);
+	
+	public int insertReviewByOject(Review review);
 	
 }
--- src/main/java/com/ajfqo/spring/ex/mybatis/service/ReviewService.java
+++ src/main/java/com/ajfqo/spring/ex/mybatis/service/ReviewService.java
@@ -20,4 +20,14 @@ public class ReviewService {
 		return review;
 	}
 	
+	public int addReview(int storeId, String menu, String userName, double point, String review) {
+		int count = reviewRepository.insertReview(storeId, menu, userName, point, review);
+		return count;
+	}
+	
+	public int addReviewByObject(Review review) {
+		int count = reviewRepository.insertReviewByOject(review);
+		return count;
+	}
+	
 }
